d85debb3b5f4250c961fa52827d2170d4ed7f2d8
belageor <belageor@blm37.iro.umontreal.ca>
2021-09-12 14:43:13 -0400

    Add global counter and timing list

--- hmw1/main.py
+++ hmw1/main.py
@@ -1,51 +1,39 @@
 import pandas as pd
-#import argparse
 import nltk
 from collections import Counter
 import glob
 import numpy as np
-import matplotlib.pyplot as plt
+#import matplotlib.pyplot as plt
+import time
 
 num_tokens = []
 num_types = []
-types = set()
+global_counter = Counter()
+time_list = []
 
-filenames = glob.glob("/u/demorali/corpora/1g-word-lm-benchmark-r13output/training-monolingual.tokenized.shuffled/*")
-#filenames = filenames[:2]
+filenames = sorted(glob.glob("/u/demorali/corpora/1g-word-lm-benchmark-r13output/training-monolingual.tokenized.shuffled/*"))
+filenames = filenames[:5]
+start_time = time.perf_counter()
 for i, filename in enumerate(filenames):
-    print(i)
+    print(filename,i)
 
-#if __name__=="__main__":
-    #import pdb; pdb.set_trace()
-    #parser = argparse.ArgumentParser(description='Count words')
-    #parser.add_argument('filename', metavar="fn", type=str, help='file to analyze')
-    #filenames = glob
-    #filename = "/u/demorali/corpora/1g-word-lm-benchmark-r13output/training-monolingual.tokenized.shuffled/news.en-00001-of-00100"
-    #df = pd.read_csv(filename, sep="\t")
-    #for line in pd.read_csv(filename, encoding='utf-8', header=None, chunksize=1):
-    #    lines.append(line.iloc[0,0])
     with open(filename, 'r') as f:
         lines = f.readlines()
     doc_tokens = 0
     
     for line in lines:
-        tokens = Counter(line.split())
-            #print(tokens)
-            #import pdb; pdb.set_trace()
-            #num_types.append(len(tokens))
-            #num_tokens.append(sum(tokens.values()))
-        doc_tokens += sum(tokens.values())
-        for el in tokens.keys():
-            if el not in types:
-                types.add(el)
-    num_tokens.append(doc_tokens)
-    num_types.append(len(types))
-
-print("The total number of tokens is: ", sum(num_tokens))
-print("The total number of types is: ", sum(num_types))
+        line_counter = Counter(line.split())
+        global_counter.update(line_counter)
+    num_tokens.append(sum(global_counter.values()))
+    num_types.append(len(global_counter))
+    ctime = time.perf_counter()
+    time_list.append(ctime-start_time)
+print("The total number of tokens is: ", num_tokens[-1])
+print("The total number of types is: ", num_types[-1])
 print('I love DIRO')
 print(num_tokens)
 print(num_types)
+print(time_list)
 #plt.plot(num_tokens)
 #plt.ylabel('counts')
 #plt.show()
@@ -56,3 +44,7 @@ with open('num_tokens.txt', 'w') as nt:
 with open('num_types.txt', 'w') as nty:
     for item in num_types:
         nty.write("%s\n" % item)
+
+with open('time_list.txt', 'w') as tl:
+    for item in time_list:
+        tl.write("%s\n" % item)
