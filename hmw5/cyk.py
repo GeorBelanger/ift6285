6b871d52eedb1212967ac3b29c1d6f367533b8a5
belageor <belageor@blm45.iro.umontreal.ca>
2021-10-29 14:43:58 -0400

    add boilerplate code, todo code get_missing_words

--- hmw5/cyk.py
+++ hmw5/cyk.py
@@ -1,23 +1,26 @@
-import datetime
+# Based on https://www.cs.bgu.ac.il/~elhadad/nlp16/NLTK-PCFG.html and the code that prof Phillipe Langlais showed during IFT6285 NLP course
+
+def get_missing_words(grammar, sent):
+    import pdb; pdb.set_trace()
+    return (None, None)
+
+import time
 import sys
-# Based in the code that prof Phillipe Langlais showed during IFT6285 NLP course
 import nltk
-from nltk.corpus import treebank
+from nltk.corpus import treebank # install ntlk and data
 from nltk import Nonterminal
 from nltk import induce_pcfg
 # from nltk.parse import ViterbiParse
 from nltk.parse import ViterbiParser
 from collections import Counter
 
-# def get_args():
-#     di = {'verbosity': 4, 'top': 3, 'max': 15, 'min': 5}
-#     return di
-# args = get_args()
 args = {'verbosity': 4, 'top': 3, 'max': 15, 'min': 5}
 
 # import pdb; pdb.set_trace()
 train = treebank.fileids()[:190]
 test = treebank.fileids()[190:]
+## TODO: analyze gramatically problematic sentences from the Cola dev corpus (those marked with a star)
+# /home/www-labs/felipe/public_html/IFT6285-Automne2021/CoLA/
 
 productions = []
 sent = 1
@@ -30,7 +33,7 @@ for item in train:
         #tree.collapse_unary(collapsePOS = False) # REmove branches A-B-C into A-B+C
         #tree.chomsky_normal_form(horzMarkov = 2) # Remove A -> (B,C,D) into A->B,C+D
 
-        productions += tree.productions()
+        productions += tree.productions() # tree.productions() returns the list of CFG rules that "explain" the tree
         # if args.verbosity > 3:
         if args['verbosity']> 3:
             for i,p in enumerate(tree.productions()):
@@ -48,7 +51,7 @@ prods = [p for p in productions if p in proto]
 # if args.unk is None:
 unk = None
 if not unk is None:
-    lexical_symbols = set(map(lambda p: p.lhs(), filter(lambda p: p.is_lexical())))
+    lexical_symbols = set(map(lambda p: p.lhs(), filter(lambda p: p.is_lexical()))) # tree.productions() will return a production with the parent node as LHS and the children as RHS.
     for lhs in lexical_symbols:
         # p=Production(lhs,[args.unk])
         p=Production(lhs,[unk])
@@ -56,19 +59,19 @@ if not unk is None:
 
 # train the grammar:
 axiom = Nonterminal('S')
-grammar = induce_pcfg(axiom, prods)
-print(f'#productions initial: {len(productions)} final: {len(grammar.productions)}')
+grammar = induce_pcfg(axiom, prods) # induce PCFG grammar from treebank data
+print(f'#productions initial: {len(productions)} final: {len(grammar.productions())}')
 
 # if args.verbosity > 4:
 if args['verbosity'] > 4:
     print(grammar)
 
 # now parse
-parse = ViterbiParser(grammar)
+parser= ViterbiParser(grammar)
 
 
 ####
-
+acc=0
 nbtest=0 # nb of test sentences
 treated = 0 # nb of sentences parsed
 unparsed = 0 # nb of sentences withouth parse
@@ -84,15 +87,16 @@ for item in test:
                 treated += 1
 
                 print("===", file=sys.stderr)
-                nb_unk, sent_unk = get_missing_words(grammar, sent)
+                
+                nb_unk, sent_unk = get_missing_words(grammar, sent) # https://stackoverflow.com/questions/35103191/nltk-viterbiparser-fails-in-parsing-words-that-are-not-in-the-pcfg-rule
 
                 print(f'sent ({len(sent)})=>', " ".join(sent), file = sys.stderr)
                 if nb_unk:
                     print(f'sent: {nbtest} missing words: {nb_unk}/{len(sent)}')
 
-                start_time = datetime.now()
+                start_time = time.perf_counter()
                 parses = parser.parse_all(sent_unk)
-                time_elapsed = datetime.now() - start_time
+                time_elapsed = time.perf_counter - start_time
 
                 if len(parses) > 0:
                     best = parses[0]
